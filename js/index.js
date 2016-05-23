c942fe98f77b729c671c02b7caa7586890db1786
Aung Myo Kyaw <amk.maeu@gmail.com>
2016-05-23 21:34:49 +0630

    nay num

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var result=document.getElementById('result');
+var textval=document.getElementById('textval');
+var h1=document.getElementsByTagName('h1');
+var text=document.getElementById('textbox')
+text.onkeyup=function(){
+	if(this.value!==""){
+		console.log(this.value);
+		result.innerHTML=nayNum(this.value.toString());
+		textval.innerHTML=numFormat(result.innerHTML);
+	} else {
+		result.innerHTML="";
+		textval.innerHTML="";
+	}
+};
